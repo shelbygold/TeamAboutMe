9740c8c7dbe612b41b3de3298149e1558bf8edcc
shelby gold <shelbygold@shelbys-MacBook-Air.local>
2019-04-15 13:26:32 -0600

    test

---
+++ BestGroupAboutMe/CollectionView/TeamMemberCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TeamMemberCollectionViewCell.swift
+//  BestGroupAboutMe
+//
+//  Created by shelby gold on 4/15/19.
+//  Copyright © 2019 shelby gold. All rights reserved.
+//
+
+import UIKit
+
+class TeamMemberCollectionViewCell: UICollectionViewCell {
+    
+}
